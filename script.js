6319b53c665ad5d0b5eb35eab8a8e612addb9848
DouglasCBS <douglascesar12@gmail.com>
2023-06-06 10:37:44 -0300

    make a function to play randomly after the player
    make a function to play first if the player chooses "O"

--- script.js
+++ script.js
@@ -10,6 +10,7 @@
             let i = 0;
 
             if (winnerMark == player.mark) {winner = player.name}
+            else if (winnerMark != player.mark) {winner = "CPU"}
             
             if (isVictory(cells)) {
                 alert(`${winner} wins!`);
@@ -44,12 +45,14 @@
         const displayGame = (player) => {
             for (i = 0; i < 9; i++) {
                 div[i] = document.createElement("div");
+                div[i].classList.add(`class-${i}`);
                 div[i].addEventListener("click", (e) => {
                     if (e.target.textContent !== "") return;
                     else  {
                         e.target.textContent = `${player.mark}`;
-                        winnerMark = e.target.textContent;
+                        let winnerMark = e.target.textContent;
                         checkWinner(winnerMark, player);
+                        playRound(winnerMark, player);
                     }
                 });
                 main.appendChild(div[i]);
@@ -66,9 +69,17 @@
     
             const player = getPlayer(playerName, playerMark);
             displayGame(player);
-
+            getPriority(player);
             
         });
+
+        function getPriority(player) {
+            if (player.mark === "X") {return}
+            else {
+                let choice = Math.floor(Math.random() * 9);
+                document.querySelector(`.class-${choice}`).textContent = "X"
+            }
+        }
         
         function reset() {
             const divs = document.querySelectorAll("div");
@@ -78,6 +89,26 @@
 
         const getPlayer = (name, mark) => {
             return {name, mark}
+        };
+
+        function playRound(winnerMark, player) {
+            let choice = Math.floor(Math.random() * 9);
+            let enemyMark;
+            if (player.mark === "X") {enemyMark = "O"}
+            else if (player.mark === "O") {enemyMark = "X"};
+            winnerMark = enemyMark;
+            if (document.querySelector(`.class-${choice}`).textContent !== "") {
+                do {
+                    choice = Math.floor(Math.random() * 9);
+                }
+                while (document.querySelector(`.class-${choice}`).textContent !== "");
+                document.querySelector(`.class-${choice}`).textContent = `${enemyMark}`;
+                checkWinner(winnerMark, player);
+            }
+            else if (document.querySelector(`.class-${choice}`).textContent === "") {
+                document.querySelector(`.class-${choice}`).textContent = `${enemyMark}`;
+                checkWinner(winnerMark, player);
+            }
         };
 
     })();
